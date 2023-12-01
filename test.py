1afd87e9ac84c3034dbd47c47d96baa47e38fd68
kimHyoSun2 <53066364+kimHyoSun2@users.noreply.github.com>
2023-12-01 15:27:22 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+view = [100,150,80]
+view
+st.write('## test view')
+st.bar_chart(view)
+
+sview = pd.Series(view)
+sview
+# 실행하는 방법streamlit run test.py
+# 결과값 
+# [
+# 0:100
+# 1:100
+# 2:100
+# ]
